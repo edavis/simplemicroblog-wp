c68c7ad0c853bcb4aab0682f4d500294b3a66768
Eric Davis <edavis@eresources.com>
2017-06-10 15:31:31 -0700

    Drop titles entirely

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,14 +10,6 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_single() ) :
-			the_title( '<h2 class="entry-title">', '</h2>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;  ?>
-	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php
